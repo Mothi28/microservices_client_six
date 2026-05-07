d074462266c00f210250fcaab6d773a436e93e8e
Mothi28 <47380612+Mothi28@users.noreply.github.com>
2026-05-07 22:48:32 +0530

    Add files via upload
    This commit has @RateLimiter sample code.

--- OrderRestController.java
+++ OrderRestController.java
@@ -1,10 +1,15 @@
 package com.client.six;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 @RequestMapping("/order")
@@ -25,4 +30,24 @@ public class OrderRestController {
     public ResponseEntity<String> showOrderInstanceId() {
        return ResponseEntity.ok("FROM ORDER SERVICE, Instance Id is: " + instanceId);
     }
+    
+    @GetMapping("/rateLimitMethod")
+    @RateLimiter(name="getMessageRateLimit", fallbackMethod="getMessageFallBack")
+    public ResponseEntity<String> rateLimitMethod() {
+	
+    	return ResponseEntity.status(HttpStatus.ACCEPTED).body("got the request !!! ");    	
+    	
+    	//return "Printing via the rateLimitMethod ";
+    }
+    
+    public ResponseEntity<String> getMessageFallBack(RequestNotPermitted exception) {
+    	
+    	System.out.println("Inside Messgae Fallback method ");
+    	return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("too many request !!! ");    	
+    	
+    	//return "Printing via the rateLimitMethod ";
+    }
+    
+    
+    
 }
